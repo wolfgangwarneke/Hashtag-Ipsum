00787b906ffa1bfa60b1ae41e582872cb542abd8
Patrick Lipscomb, Sky Rousse and Wolfgang Warneke <sky@>
2016-06-21 11:53:04 -0700

    refactored array access for simplicity

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,16 @@
 var stockBanks = [];
 
-function Theme(variable, name, words, description) {
-  this.varName = variable;
+function Theme(name, words, description) {
+  this.bankIndex = stockBanks.length;
   this.name = name;
   this.bank = words.split(',');
   this.description = description;
 }
 
-var theme1 = new Theme('theme1', 'TrumpSum', 'Donald Trump,barley,rye,wall,ipsum,lorem,oink,orange,build the wall,Make America Great Again,Ivanka,Youtube,memes,OMG,rich,eeeyuuuuge', 'this is our template description');
+var theme1 = new Theme('TrumpSum', 'Donald Trump,barley,rye,wall,ipsum,lorem,oink,orange,build the wall,Make America Great Again,Ivanka,Youtube,memes,OMG,rich,eeeyuuuuge', 'this is our template description');
 stockBanks.push(theme1);
 
-var theme2 = new Theme('theme2', 'Clinton Ipsum', 'Stand with her,barley,rye,wall,ipsum,lorem,oink,orange,build the wall,Make America Great Again,Ivanka,Youtube,memes,OMG,rich,eeeyuuuuge', 'this is OTHER template');
+var theme2 = new Theme('Clinton Ipsum', 'Stand with her,barley,rye,wall,ipsum,lorem,oink,orange,build the wall,Make America Great Again,Ivanka,Youtube,memes,OMG,rich,eeeyuuuuge', 'this is OTHER template');
 stockBanks.push(theme2);
 
 
@@ -19,17 +19,17 @@ function loadThemeMenu() {
   var themeList = "";
   themeList += "<option selected disabled>Load an Ipsum</option>";
   for (i = 0; i < stockBanks.length; i++) {
-    themeList += "<option value='" + stockBanks[i].varName + "'>" + stockBanks[i].name + "</option>";
+    themeList += "<option value='" + stockBanks[i].bankIndex + "'>" + stockBanks[i].name + "</option>";
   }
   $('#themes').append(themeList);
 }
 
 $('#themes').change(function() {
-  $('#themeDescription p').text(window[$('#themes').val()].description);
+  $('#themeDescription p').text(stockBanks[$('#themes').val()].description);
 });
 
 $('#oneWord').on('click', function() {
-  var output = window[$('#themes').val()].bank[Math.floor(Math.random() * window[$('#themes').val()].bank.length)];
+  var output = stockBanks[$('#themes').val()].bank[Math.floor(Math.random() * stockBanks[$('#themes').val()].bank.length)];
   $('#ipsumOutput').html('<h3>' + output + '</h3>');
 });
 
