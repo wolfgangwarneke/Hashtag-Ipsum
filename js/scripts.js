46bc3995ff7e7785a511a893f6f9bf5ad85110f2
George Olson and Sky Rousse <sky@>
2016-06-23 16:46:09 -0700

    show user generator and hidse theme selecection, then vice-versa

--- js/scripts.js
+++ js/scripts.js
@@ -47,7 +47,7 @@ stockBanks['TrumpSum'] = new Theme('TrumpSum', 'Donald Trump,little hands,Wall S
 
 stockBanks['YeezIpsum'] = new Theme('YeezIpsum', 'Kanye West,Yeezus,the life of Pablo,create art,dark twisted fantasy,my beautiful,Kardashians,Kim,watch the throne,emotional over fonts,Wicked is my story,fur pillows,hobo couture,leatherpants,mic drop,king,lamborgini mercy,bank-rupt,motorcycle,Imma let you finish', 'The Ipsum of a Generation');
 
-stockBanks['Bi-Winning-Ipsum'] = new Theme('Bi-Winning-Ipsum', 'Charlie Sheen,winning,I am on a drug called Charlie Sheen,rock star from Mars,rolling out Magic,bro,sober,dying is for fools,I\'m not dangerous insane,boom,Vatican assassin warlocks,seven gram rocks,party,partying,drinking,breakfast,heavy drinking,bi-winning,duh,I\'m still alive,breakfast martini', 'Boom. Winning Ipsum. Duh!');
+stockBanks['Bi-Winning-Ipsum'] = new Theme('Bi-Winning-Ipsum', 'Charlie Sheen,winning,I am on a drug called Charlie Sheen,rock star from Mars,rolling out Magic,bro,sober,dying is for fools,I\'m not dangerous insane,boom,two donuts and a Rockstar,Vatican assassin warlocks,seven gram rocks,party,partying,drinking,breakfast,heavy drinking,bi-winning,duh,I\'m still alive,breakfast martini', 'Boom. Winning Ipsum. Duh!');
 
 var normalIpsum = "";
 
@@ -268,6 +268,16 @@ $('#paragraphs').mousedown(function() {
   });
 });
 
+
+$('#makeYourOwn').click(function() {
+  $('#ipsumGENERATOR').addClass('hide');
+  $('#userGENERATOR').removeClass('hide');
+});
+
+$('#goBack').click(function() {
+  $('#ipsumGENERATOR').removeClass('hide');
+  $('#userGENERATOR').addClass('hide');
+});
 
 ///USER GENERATOR
 
